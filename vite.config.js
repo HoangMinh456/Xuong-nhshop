7325ae54ac13f881a7c19f33b86a5f97ea6b3b09
HoangMinh456 <xinphepgiauten456@gmail.com>
2024-03-21 22:35:37 +0700

    lan 11

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,13 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve('/', "index.html"),
-                shop: resolve('/shop', "shopPage.html"),
+                main: resolve(__dirname, "index.html"),
+                shop: resolve(__dirname, "shopPage.html"),
                 cart: resolve(__dirname, "cart.html"),
                 detail: resolve(__dirname, "detailPage.html"),
                 checkout: resolve(__dirname, "checkout.html"),
-                signup: resolve(__dirname, "singupPage.html")
+                signup: resolve(__dirname, "signupPage.html")
             }
-
         },
     },
 })
